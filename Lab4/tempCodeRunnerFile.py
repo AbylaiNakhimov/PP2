c5071c37a7bc302349665ca8b6f66ba6ab8d9163
Abylai <abilai_2003@list.ru>
2023-02-19 21:55:51 +0600

    pp2

---
+++ Lab4/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+1)
+# n = int(input("Enter number: "))
+# def sqr():
+#   for i in range(1, n + 1):
+#     yield i ** 2
+# for i in sqr():
+#   print(i)
+
+# 2)
+# def ev_num(n):
+#   for i in range(0, n+1, 2):
+#     yield str(i)
+# n = int(input("Enter number: "))
+# ev_num = ev_num(n)
+# result = ','.join(ev_num)
+# print(result)
