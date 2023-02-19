e8d161eff4dad0163f22480a09db2e789d6f53db
Abylai <abilai_2003@list.ru>
2023-02-19 23:58:44 +0600

    pp2

--- Lab4/json/jsontask.py
+++ Lab4/json/jsontask.py
@@ -1,17 +1,13 @@
 import json
 
-jsondata = open('sample-data.json').read()
-
-json_object = json.loads(jsondata)
-print(
-    "=======================================================================================" "\n"
-    "DN                                                 Description           Speed    MTU" "\n" 
-    "-------------------------------------------------- --------------------  ------  ------")
-imdata = json_object["imdata"]
-for i in imdata:
-        dn = i["l1PhysIf"]["attributes"]["dn"]
-        descr = i["l1PhysIf"]["attributes"]["descr"]
-        speed = i["l1PhysIf"]["attributes"]["speed"]
-        mtu = i["l1PhysIf"]["attributes"]["mtu"]
-        # print fields formatted in columns
-        print("{0:50} {1:20} {2:7} {3:6}".format(dn,descr,speed,mtu))
+file = open('01.json', 'r')
+js = json.load(file)
+print("Interface Status\n================================================================================")
+print("DN                                                 Description           Speed    MTU  ")
+print("-------------------------------------------------- --------------------  ------  ------")
+for i in js['imdata']:
+    print(i['l1PhysIf']['attributes']['dn'], end ="                       \t")
+    print(i['l1PhysIf']['attributes']['descr'], end= "  \t")
+    print( i['l1PhysIf']['attributes']['speed'], end= "   ")
+    print( i['l1PhysIf']['attributes']['mtu'], end= '\n')
+file.close()
